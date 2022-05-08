18c63bc5104b00bcf82b208a60c7213fd40383d9
h1ze <burgov@mail.ru>
2022-05-08 22:46:53 +0400

    add Remove_String_Spaces.js

---
+++ Remove_String_Spaces.js
@@ -0,0 +1,17 @@
+// 1 ВАРИАНТ (МЕТОДЫ SPLIT и JOIN)
+
+function noSpace(x){
+    return x.split(' ').join('');
+}
+
+console.log(noSpace('Проверяем   работу            функции.'))
+
+
+// 2 ВАРИАНТ (МЕТОД REPLACE + РЕГУЛЯРКА)
+
+
+function noSpace2(x){
+    return x.replace(/\s/g, '');
+  }
+
+  console.log(noSpace2('Проверяем    работу   функции.'))
