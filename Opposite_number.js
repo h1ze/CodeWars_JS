d0135d51c8b3c7937dab6809f88d80ff46ee1f16
h1ze <burgov@mail.ru>
2022-05-12 20:27:42 +0400

    create Opposite_number.js

---
+++ Opposite_number.js
@@ -0,0 +1,18 @@
+// Very simple, given an integer or a floating-point number, find its opposite.
+
+// Вариант 1. (Умножаем аргумент на -1)
+
+function opposite(number) {
+    return number = number * -1;
+  }
+
+console.log(opposite(15))
+
+// Вариант 2 (Можно обойтись без умножения на -1, возвращая сразу аргумент с -)
+
+function opposite2(number2) {
+    return(-number2);
+  }
+
+console.log(opposite(-23))
+
