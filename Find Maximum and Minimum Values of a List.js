cae6334e5c91c5a36500343848da005eca9ead52
h1ze <burgov@mail.ru>
2022-06-02 17:19:14 +0400

    create

---
+++ Find Maximum and Minimum Values of a List.js
@@ -0,0 +1,36 @@
+// Your task is to make two functions (max and min, or maximum and minimum, etc., depending on the language) that receive a list of integers 
+// as input and return, respectively, the largest and lowest number in that list.
+
+// Examples (Input -> Output)
+// * [4,6,2,1,9,63,-134,566]         -> max = 566, min = -134
+// * [-52, 56, 30, 29, -54, 0, -110] -> min = -110, max = 56
+// * [42, 54, 65, 87, 0]             -> min = 0, max = 87
+// * [5]                             -> min = 5, max = 5
+// Notes
+// You may consider that there will not be any empty arrays/vectors.
+
+var min = function(list){
+    
+    return list.sort((a,b) => a - b)[0];
+}
+
+var max = function(list){
+    
+    return list.sort((a,b) => a - b).reverse()[0]; // list.sort((a, b) => (b - a));
+}
+
+console.log(min([42, 54, 65, 87, 0]))
+console.log(max([4,6,2,1,9,63,-134,566]))
+
+
+const min2 = (list) => Math.min(...list);
+const max2 = (list) => Math.max(...list);
+
+var min3 = function(list){
+    return Math.min.apply(null,list);
+}
+
+var max3 = function(list){
+    return Math.max.apply(null,list);
+}
+
