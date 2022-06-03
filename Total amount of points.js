fff4abe9d1af705bf5e06b7411036f92e2bd8091
h1ze <burgov@mail.ru>
2022-06-03 18:45:46 +0400

    create

---
+++ Total amount of points.js
@@ -0,0 +1,39 @@
+// Our football team finished the championship. The result of each match look like "x:y". Results of all matches are recorded in the collection.
+
+// For example: ["3:1", "2:2", "0:1", ...]
+
+// Write a function that takes such collection and counts the points of our team in the championship. Rules for counting points for each match:
+
+// if x > y: 3 points
+// if x < y: 0 point
+// if x = y: 1 point
+// Notes:
+
+// there are 10 matches in the championship
+// 0 <= x <= 4
+// 0 <= y <= 4
+
+function points(games) {
+    let table = 0;
+    games.forEach((a) => { 
+        a[0] > a[2] ? table += 3 : a[0] == a[2] ? table += 1 : table;
+    });
+    return table;
+}
+
+  console.log(points(["1:0","2:0","3:0","4:4","0:1"]));
+
+  
+
+  const points2=games=>games.reduce((output,current)=>{
+    return output += current[0]>current[2] ? 3 : current[0]===current[2] ? 1 : 0;
+  },0)
+
+// function points(games) {
+//   return games.reduce((output,current)=>{
+//     let x = parseInt(current[0]);
+//     let y = parseInt(current[2]);
+//     let value= x>y ? 3 : x===y ? 1 : 0;
+//     return output+value;
+//   },0)
+// }
