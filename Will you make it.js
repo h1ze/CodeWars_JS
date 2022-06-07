b9c024c36d77221f7809faf8aac0260852628ca8
h1ze <burgov@mail.ru>
2022-06-07 18:09:56 +0400

    fixed

--- Will you make it.js
+++ Will you make it.js
@@ -2,6 +2,7 @@
 // pump is 50 miles away! You know that on average, your car runs on about 25 miles per gallon. There are 2 gallons left. Considering these 
 // factors, write a function that tells you if it is possible to get to the pump or not. Function should return true (1 in Prolog, NASM and COBOL) 
 // if it is possible and false (0 in Prolog, NASM and COBOL) if not. The input values are always positive.
+
 const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
     return mpg * fuelLeft >= distanceToPump;
   };
